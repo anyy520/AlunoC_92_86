a668b70172c2ee7458f4a24fd241a981fa6743fd
AnyySann <86086338+anyy520@users.noreply.github.com>
2023-10-05 01:23:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Crie o elemento canvas:
+
+// defina a width e height do bloco imagem
+
+// defina as coordenadas x e y da imagem do personagem
+
+// Função para adicionar playerUpdate():
+
+// Função para adicionar a imagem do bloco
+
